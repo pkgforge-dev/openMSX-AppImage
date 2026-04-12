5bb08cc3208053735572a41e637020d153937b4b
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:18:37 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,20 +3,15 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q openmsx | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/openmsx/icons/openMSX-logo-256.png
-export DESKTOP=/usr/share/applications/openmsx.desktop
-export STARTUPWMCLASS=openmsx
+export ICON=https://raw.githubusercontent.com/openMSX/openMSX/refs/heads/master/share/icons/openMSX-logo-256.png
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/openmsx /usr/share/openmsx
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
