e1941db416699b213af18a602b0d5907dd370f2e
Link4Electronics <link4electronics@gmail.com>
2026-08-25 21:08:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q openmsx | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q openmsx | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -16,11 +16,5 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/openmsx /usr/share/openmsx
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the app normally quits before that time
-# then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --simple-test ./dist/*.AppImage
