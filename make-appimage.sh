7eaeea597c825e3854f21e83d97ec78cb0ce94a5
Link4Electronics <link4electronics@gmail.com>
2026-08-30 12:44:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/openmsx/icons/openMSX-logo-256.png
 export DESKTOP=/usr/share/applications/openmsx.desktop
 export STARTUPWMCLASS=openmsx
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/openmsx /usr/share/openmsx
