83baa45bbc3d86a26ccd54916d2ad64c71648762
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:34:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,3 +18,7 @@ quick-sharun /usr/bin/openmsx /usr/share/openmsx
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
