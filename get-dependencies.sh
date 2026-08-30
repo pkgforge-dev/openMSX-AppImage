e4fedf9e22643495b24dabf7cb5aa34757e4738f
Link4Electronics <link4electronics@gmail.com>
2026-08-30 12:44:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,20 +6,38 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    glew      \
+    libtheora \
+    sdl2_ttf  \
+    tcl
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
-make-aur-package openmsx
+#make-aur-package openmsx
 
 # If the application needs to be manually built that has to be done down here
-#if [ "${DEVEL_RELEASE-}" = 1 ]; then
-#    package=openmsx-git
-#else
-#    package=openmsx
-#fi
-#make-aur-package "$package"
-#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+echo "Building stable version of openMSX..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/openMSX/openMSX"
+TAG="$(curl -s https://api.github.com/repos/openMSX/openMSX/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)"
+VERSION="${TAG#RELEASE_}"
+git clone "$REPO" ./openMSX
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./openMSX
+git checkout "$TAG"
+sed -i 's@SYMLINK_FOR_BINARY:=true@SYMLINK_FOR_BINARY:=false@' build/custom.mk
+sed -i 's@INSTALL_BASE:=/opt/openMSX@INSTALL_BASE:=/usr/share/openmsx@' build/custom.mk
+echo 'INSTALL_DOC_DIR:=/usr/share/doc/openmsx' >> build/custom.mk
+echo 'INSTALL_SHARE_DIR:=/usr/share/openmsx' >> build/custom.mk
+echo 'INSTALL_BINARY_DIR:=/usr/bin' >> build/custom.mk
+
+# Compiling
+./configure
+make -j$(nproc)
+make install
