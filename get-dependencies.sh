07818007bab7147ded83f327f0a6d2bdf3460ba9
Link4Electronics <link4electronics@gmail.com>
2026-08-30 18:12:41 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,10 +16,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package openmsx
-
-# If the application needs to be manually built that has to be done down here
 echo "Building stable version of openMSX..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/openMSX/openMSX"
